0638bd253979fe6430a953d311bb05e123b25c94
Frank Hale <frankhale@gmail.com>
2024-11-19 21:12:37 -0500

    remove unused ngTemplateOutlet directive

--- src/app/question-input-date/question-input-date.component.ts
+++ src/app/question-input-date/question-input-date.component.ts
@@ -5,23 +5,21 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatInputModule} from '@angular/material/input';
 import {provideNativeDateAdapter} from '@angular/material/core'
 import {ControlType, QuestionInputComponent} from '../component/question-input-base-component';
-import {NgTemplateOutlet} from '@angular/common';
 
 @Component({
-    selector: 'app-question-input-date',
-    providers: [provideNativeDateAdapter(),
-        { provide: QuestionInputComponent, useExisting: QuestionInputDateComponent }],
-    imports: [
-        FormsModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatLabel,
-        MatDatepickerModule,
-        ReactiveFormsModule,
-        NgTemplateOutlet,
-    ],
-    templateUrl: './question-input-date.component.html',
-    styleUrl: './question-input-date.component.scss'
+  selector: 'app-question-input-date',
+  providers: [provideNativeDateAdapter(),
+    {provide: QuestionInputComponent, useExisting: QuestionInputDateComponent}],
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatLabel,
+    MatDatepickerModule,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './question-input-date.component.html',
+  styleUrl: './question-input-date.component.scss'
 })
 export class QuestionInputDateComponent extends QuestionInputComponent<string> {
   controlType: ControlType = 'date';
